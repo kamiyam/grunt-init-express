3aa0a199db77d2550c673561a8812d93ef079166
kamiyam <labs.k5m@gmail.com>
2013-07-01 21:20:10 +0900

    use grunt-express-server

--- template.js
+++ template.js
@@ -1,7 +1,7 @@
 /**
  * grunt-init-express
- * 
- * @version      v0.0.1rc1
+ *
+ * @version      v0.0.1rc2
  * @author       kamiyam (http://twitter.com/kamiyam)
  * @copyright    (c) 2013 "kamiyam"
  * @license      The MIT License
@@ -16,11 +16,11 @@ exports.description = 'Create a Express apps plugin.';
 
 // about express
 exports.notes = 'For more information about Express, ' +
-  'please see the docs at http://expressjs.com/guide.html';
+    'please see the docs at http://expressjs.com/guide.html';
 
 // after setup express
 exports.after = 'You should now install project dependencies with _npm ' +
-  'install_. After comand _express app_';
+    'install_. After comand _express app_';
 
 // If exist files warning.
 exports.warnOn = '*';
@@ -28,61 +28,63 @@ exports.warnOn = '*';
 // template files
 exports.template = function(grunt, init, done) {
 
-  init.process({type: 'grunt'}, [
-    // prompt
-    init.prompt('name', 'Express Application name'),
-    init.prompt('description', 'Express Application description'),
-    init.prompt('version'),
-    init.prompt('repository'),
-    init.prompt('homepage'),
-    init.prompt('bugs'),
-    init.prompt('licenses'),
-    init.prompt('author_name'),
-    init.prompt('author_email'),
-    init.prompt('author_url'),
-    init.prompt('grunt_version'),
-    init.prompt('node_version', grunt.package.engines.node)
-  ], function(err, props) {
+    init.process({type: 'grunt'}, [
+        // prompt
+        init.prompt('name', 'Express Application name'),
+        init.prompt('description', 'Express Application description'),
+        init.prompt('version'),
+        init.prompt('repository'),
+        init.prompt('homepage'),
+        init.prompt('bugs'),
+        init.prompt('licenses'),
+        init.prompt('author_name'),
+        init.prompt('author_email'),
+        init.prompt('author_url'),
+        init.prompt('grunt_version'),
+        init.prompt('node_version', grunt.package.engines.node),
+        init.prompt('express_prameter',"")
 
-    props.short_name = props.name;
-    props.main = 'Gruntfile.js';
-    props.npm_test = 'grunt test';
-    props.keywords = ['grunt','node.js','express'];
-    props.devDependencies = {
-    "matchdep": "~0.1.1",
-    "grunt": "~0.4.1",
-    "grunt-regarde": "~0.1.1",
-    "grunt-contrib-connect": "~0.2.0",
-    "grunt-contrib-livereload": "~0.1.2",
-    "grunt-open": "~0.2.0",
-    "grunt-connect-proxy": "~0.1.0",
-    "grunt-exec": "~0.4.0",
-    "grunt-contrib-coffee": "~0.6.3",
-    "grunt-contrib-compass": "~0.1.3",
-    "grunt-contrib-clean": "~0.4.0",
-    "grunt-contrib-copy": "~0.4.0",
-    "grunt-contrib-concat": "~0.1.3",
-    "grunt-contrib-mincss": "~0.4.0rc7",
-    "grunt-contrib-uglify": "~0.2.0",
-    "supervisor": "~0.5.2",
-    };
-    props.peerDependencies = {
-      'grunt': props.grunt_version,
-    };
+    ], function(err, props) {
 
-    // filecopy
-    var files = init.filesToCopy(props);
+        props.short_name = props.name;
+        props.main = 'Gruntfile.js';
+        props.npm_test = 'grunt test';
+        props.keywords = ['grunt','node.js','express'];
+        props.devDependencies = {
+            "matchdep": "~0.1.2",
+            "grunt": "~0.4.1",
+            "grunt-regarde": "~0.1.1",
+            "grunt-contrib-connect": "~0.3.0",
+            "grunt-contrib-livereload": "~0.1.2",
+            "grunt-open": "~0.2.0",
+            "grunt-connect-proxy": "~0.1.4",
+            "grunt-exec": "~0.4.2",
+            "grunt-contrib-coffee": "~0.7.0",
+            "grunt-contrib-compass": "~0.3.0",
+            "grunt-contrib-clean": "~0.4.1",
+            "grunt-contrib-copy": "~0.4.1",
+            "grunt-contrib-concat": "~0.3.0",
+            "grunt-contrib-mincss": "~0.3.2",
+            "grunt-contrib-uglify": "~0.2.2",
+            "grunt-express-server": "~0.4.0"
+        };
+        props.peerDependencies = {
+            'grunt': props.grunt_version
+        };
 
-    // add lisences file
-    init.addLicenseFiles(files, props.licenses);
+        // filecopy
+        var files = init.filesToCopy(props);
 
-    // setting all files
-    init.copyAndProcess(files, props);
+        // add lisences file
+        init.addLicenseFiles(files, props.licenses);
 
-    // create package.json
-    init.writePackageJSON('package.json', props);
+        // setting all files
+        init.copyAndProcess(files, props);
 
-    done();
-  });
+        // create package.json
+        init.writePackageJSON('package.json', props);
+
+        done();
+    });
 
 };
